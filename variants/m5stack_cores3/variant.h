affb3a29faed1f8460e16d016a90ce504579e108
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-09-22 11:00:11 +0200

    Update variant.h

--- variants/m5stack_cores3/variant.h
+++ variants/m5stack_cores3/variant.h
@@ -8,14 +8,19 @@
 #define LORA_MOSI 37
 #define LORA_CS 1
 
-#define TFT_HEIGHT 240
-#define TFT_WIDTH 320
-#define TFT_OFFSET_X 0
-#define TFT_OFFSET_Y 0
-#define TFT_BUSY -1
+// Leave undefined to disable our PMU IRQ handler.  DO NOT ENABLE THIS because the pmuirq can cause sperious interrupts
+// and waking from light sleep
+// #define PMU_IRQ 40
+#define HAS_AXP2101
+
+//#define TFT_HEIGHT 240
+//#define TFT_WIDTH 320
+//#define TFT_OFFSET_X 0
+//#define TFT_OFFSET_Y 0
+//#define TFT_BUSY -1
 
 // LCD screens are slow, so slowdown the wipe so it looks better
-#define SCREEN_TRANSITION_FRAMERATE 1 // fps
+//#define SCREEN_TRANSITION_FRAMERATE 1 // fps
 
 // ILI9342C
-#define ILI9341_SPI_HOST VSPI_HOST // VSPI_HOST or HSPI_HOST
+//#define ILI9341_SPI_HOST VSPI_HOST // VSPI_HOST or HSPI_HOST
